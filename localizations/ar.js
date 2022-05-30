ea9d0a36319b29790813c958b044aaf5770a6162
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-05-31 00:33:38 +0300

    localizations

--- localizations/ar.js
+++ localizations/ar.js
@@ -1,5 +1,58 @@
 export default async (context, locale) => {
   return await Promise.resolve({
-    home: {},
+    home: {
+      title: "جامعة صلاح الدين",
+      description:
+        "الموقع الرسمي لأطروحات جامعة صلاح الدين ، يمكنك هنا العثور على جميع الأوراق البحثية الرسمية للبكالوريوس والدكتوراه والماجستير للطلاب المتخرجين.",
+      search: "البحث",
+      searchWithDots: "البحث...",
+      collagesTitle: "كليات جامعة صلاح الدين",
+      collages: {
+        science: "كلية العلوم",
+        engineering: "كلية الهندسة",
+        law: "كلية الحقوق",
+        agriculture: "كلية الزراعة",
+        education: "كلية التربية",
+        arts: "كلية الفنون",
+        languages: "كلية اللغات",
+        administrationAndEconomics: "كلية الإدارة والاقتصاد",
+        basicEducation: "كلية التربية الاساسية",
+        physicalEducation: "كلية التربية الرياضية",
+        islamicScience: "كلية العلوم الإسلامية",
+        fineArts: "كلية الفنون الجميلة",
+        educationShaqlawa: "كلية التربية شقلاوة",
+        educationMakhmur: "كلية التربية مخمور",
+        politicalScience: "كلية العلوم السياسية",
+      },
+      thesisDegreeNewsTitle: "أحدث درجات الرسالة",
+      collageOf: "كلية",
+      fullArticle: "انظر جميع المقالات",
+      seeFullArticle: "انظر جميع المقالات",
+      latestNews: "أحدث الأخبار",
+      thesisDegreeNewsTitle2: "عن جامعة صلاح الدين",
+      thesisDetail:
+        "جامعة صلاح الدين - أربيل هي واحدة من مؤسسات التعليم العالي الحكومية في شمال العراق وخاصة إقليم كردستان. تقع في أربيل ، عاصمة إقليم الأكراد المتمتع بالحكم الذاتي في العراق. تأسست جامعة السليمانية - السليمانية عام 1968.",
+      mainWebsite: "الموقع الرئيسي",
+      footer: "@2022 جامعة صلاح الدين أطروحة. كل الحقوق محفوظة.",
+      notFound: "الصفحة غير موجودة",
+      pageName: "موقع جامعة صلاح الدين للرسائل الجامعية",
+    },
+    thesis: {
+      title: "تصفية البحث",
+      searchPlaceholder: "البحث...",
+      allDegreesPlaceholder: "كل الدرجات",
+      allDepartments: "جميع الإدارات",
+      pageTitle: "أطروحات",
+      pageName: "موقع جامعة صلاح الدين للرسائل الجامعية",
+      pageDescription:
+        "صفحة رسائل جامعة صلاح الدين ، تصفية حسب الدرجة ، القسم ، الكلية ، البحث ، والمزيد",
+    },
+    thesisDetail: {
+      contactInfo: "معلومات الاتصال بالطالب",
+      name: "اسم",
+      email: "البريد الإلكتروني",
+      phone: "هاتف",
+      similarResearches: "ابحاث مماثلة",
+    },
   });
 };
--- localizations/en.js
+++ localizations/en.js
@@ -1,5 +1,58 @@
 export default async (context, locale) => {
   return await Promise.resolve({
-    home: {},
+    home: {
+      title: "Salahaddin University Thesis",
+      description:
+        "Official salahaddin university thesis website, here you can find all the official Bachelor, PhD, and Masters research papers of the graduated students.",
+      search: "Search",
+      searchWithDots: "Search...",
+      collagesTitle: "Salahaddin University Colleges",
+      collages: {
+        science: "College of Science",
+        engineering: "College of Engineering",
+        law: "College of Law",
+        agriculture: "College of Agriculture",
+        education: "College of Education",
+        arts: "College of Arts",
+        languages: "College of Languages",
+        administrationAndEconomics: "College of Administration & Economics",
+        basicEducation: "College of Basic Education",
+        physicalEducation: "College of Physical Education",
+        islamicScience: "College of Islamic Science",
+        fineArts: "College of Fine Arts",
+        educationShaqlawa: "College of Education Shaqlawa",
+        educationMakhmur: "College of Education Makhmur",
+        politicalScience: "College of Political Science",
+      },
+      thesisDegreeNewsTitle: "Latest Thesis Degrees",
+      collageOf: "Collage Of",
+      fullArticle: "See All Articles",
+      seeFullArticle: "See Full Article",
+      latestNews: "Latest News",
+      thesisDegreeNewsTitle2: "About Salahaddin University",
+      thesisDetail:
+        " Salahaddin University-Erbil is one of the public higher education institution in the North of Iraq and especially Kurdistan region. It is located in Erbil, capital of the autonomous Kurdis Region of Iraq. Sulaimani University-Sulaimani was established in 1968.",
+      mainWebsite: "SUE Main Website",
+      footer: "@2022 Salahaddin University Thesis. All Rights Reserved.",
+      notFound: "Page Not Found",
+      pageName: "Salahaddin University Theses Website",
+    },
+    thesis: {
+      title: "Search & Filter",
+      searchPlaceholder: "Search...",
+      allDegreesPlaceholder: "All Degrees",
+      allDepartments: "All Departments",
+      pageTitle: "Theses",
+      pageName: "Salahaddin University Theses Website",
+      pageDescription:
+        "Salahaddin University Theses page, Filter by degree, department, collage, search, and more",
+    },
+    thesisDetail: {
+      contactInfo: "Student Contact Info",
+      name: "Name",
+      email: "Email",
+      phone: "Phone",
+      similarResearches: "Similar Researches",
+    },
   });
 };
