1397f9f08729a745405ba7b2006cac44e5b47671
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-06-03 16:02:33 +0300

    fixed degree name bugs

--- components/DegreesGraduateNumbers.vue
+++ components/DegreesGraduateNumbers.vue
@@ -30,6 +30,18 @@ export default {
     },
   },
 
+  watch: {
+    degrees: function () {
+      if (!this.degrees.find((degree) => degree.degree_id === 4)) {
+        this.degrees.unshift({
+          degree_id: 4,
+          degree_name: this.$t("home.bachelor"),
+          count: 0,
+        });
+      }
+    },
+  },
+
   mounted() {
     // if this.degrees doesn't containt degrees_id 4, then add it
     if (!this.degrees.find((degree) => degree.degree_id === 4)) {
